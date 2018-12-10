b08fb035f017a22c992dc363d2e20d70105cd5bb
김이삭 <kimisaak0@gmail.com>
2018-12-11 03:19:05 +0900

    피곤해...

--- TBasisSample/Library/mathHeader_L.cpp
+++ TBasisSample/Library/mathHeader_L.cpp
@@ -12,6 +12,231 @@ namespace Lypi
 		x = _x; y = _y;
 	}
 
+	//생성자
+	Vector2::Vector2()
+		: float2() {}
+
+	Vector2::Vector2(float x)
+		: float2(x, x) {}
+
+	Vector2::Vector2(float x, float y)
+		: float2(x, y) {}
+
+	Vector2::Vector2(const float2& F)
+	{
+		x = F.x; 
+		y = F.y; 
+	}
+
+	Vector2::Vector2(const Vector2& V)
+	{
+		x = V.x; 
+		y = V.y; 
+	}
+
+	bool Vector2::operator== (const Vector2& V) const 
+	{
+		if (x == V.x && y == V.y) {
+			return true; 
+		} 
+		return false; 
+	}
+
+	bool Vector2::operator!= (const Vector2& V) const 
+	{
+		if (x == V.x && y == V.y) {
+			return false; 
+		} 
+		return true; 
+	}
+
+	Vector2& Vector2::operator+= (const Vector2& V) 
+	{
+		Vector2 rv = { x += V.x, y += V.y };
+		return rv;
+	}
+
+	Vector2& Vector2::operator-= (const Vector2& V) 
+	{
+		Vector2 rv = { x -= V.x, y -= V.y};
+		return rv; 
+	}
+
+	Vector2& Vector2::operator*= (float S)
+	{
+		Vector2 rv = { x *= S, y *= S };
+		return rv; 
+	}
+
+	Vector2& Vector2::operator/= (float S) 
+	{
+		Vector2 rv = { x /= S, y /= S };
+		return rv; 
+	}
+
+	Vector2 Vector2::operator+ () const 
+	{
+		return *this; 
+	}
+
+	Vector2 Vector2::operator- () const 
+	{
+		return Vector2(-x, -y); 
+	}
+
+	float Vector2::Length() const 
+	{
+		return sqrtf(x*x + y * y); 
+	}
+
+	float Vector2::LengthSquared() const 
+	{
+		return x * x + y * y; 
+	}
+
+	float Vector2::Dot(const Vector2& V) const 
+	{
+		return x * V.x + y * V.y; 
+	}
+
+	Vector2 Vector2::Normalize() 
+	{
+		Vector2 bf = *this; 
+
+		this->x /= Length();
+		this->y /= Length();
+		
+		return bf; 
+	}
+
+	void Vector2::Normalize(Vector2& result) const 
+	{
+		result.x /= Length(); 
+		result.y /= Length(); 
+	}
+
+	Vector2 Vector2::Clamp(const Vector2& min, const Vector2& max)
+	{
+		Vector2 bf = *this;
+		(x <= min.x) ? (x = min.x) : ((x > max.x) ? (x = max.x) : (x));
+		(y <= min.y) ? (y = min.y) : ((y > max.y) ? (y = max.y) : (y));
+		return bf;
+	}
+
+	void Vector2::Clamp(const Vector2& min, const Vector2& max, Vector2& ret)
+	{
+		(ret.x <= min.x) ? (ret.x = min.x) : ((ret.x > max.x) ? (ret.x = max.x) : (ret.x));
+		(ret.y <= min.y) ? (ret.y = min.y) : ((ret.y > max.y) ? (ret.y = max.y) : (ret.y));
+	}
+
+	//static
+	Vector2 operator+ (const Vector2& V1, const Vector2& V2)
+	{
+		return Vector2(V1.x + V2.x, V1.y + V2.y);
+	}
+
+	Vector2 operator- (const Vector2& V1, const Vector2& V2) 
+	{
+		return Vector2(V1.x - V2.x, V1.y - V2.y);
+	}
+
+	Vector2 operator* (float S, const Vector2& V)
+	{
+		return Vector2(V.x * S, V.y * S);
+	}
+
+	Vector2 operator* (const Vector2& V, float S)
+	{
+		return Vector2(V.x * S, V.y * S);
+	}
+
+	Vector2 operator/ (float S, const Vector2& V)
+	{
+		return Vector2(V.x / S, V.y / S);
+	}
+
+	Vector2 operator/ (const Vector2& V, float S) 
+	{
+		return Vector2(V.x / S, V.y / S);
+	}
+
+	float Vector2::Dot(const Vector2& v1, const Vector2& v2) 
+	{
+		return v1.x * v2.x + v1.y * v2.y; 
+	}
+
+	float Vector2::Distance(const Vector2& v1, const Vector2& v2)
+	{
+		return (v2 - v1).Length(); 
+	}
+
+	float Vector2::DistanceSquared(const Vector2& v1, const Vector2& v2) 
+	{
+		return (v2 - v1).LengthSquared(); 
+	}
+
+	Vector2 Vector2::Min(const Vector2& v1, const Vector2& v2)
+	{
+		Vector2 ret;
+		(v1.x <= v2.x) ? (ret.x = v1.x) : (ret.x = v2.x);
+		(v1.y <= v2.y) ? (ret.y = v1.y) : (ret.y = v2.y);
+		return ret;
+	}
+
+	Vector2 Vector2::Max(const Vector2& v1, const Vector2& v2)
+	{
+		Vector2 ret;
+		(v1.x >= v2.x) ? (ret.x = v1.x) : (ret.x = v2.x);
+		(v1.y >= v2.y) ? (ret.y = v1.y) : (ret.y = v2.y);
+		return ret;
+	}
+
+	//선형 보간법
+	Vector2 Vector2::Lerp(const Vector2& v1, const Vector2& v2, float t)
+	{
+		Vector2 ret;
+		t = (t > 1.0f) ? 1.0f : ((t < 0.0f) ? 0.0f : t);
+		ret.x = v1.x + t * (v2.x - v1.x);
+		ret.y = v1.y + t * (v2.y - v1.y);
+		return ret;
+	}
+
+	//스무스스텝. (필요한가..?)
+	Vector2 Vector2::SmoothStep(const Vector2& v1, const Vector2& v2, float t)
+	{
+		Vector2 ret;
+		t = (t > 1.0f) ? 1.0f : ((t < 0.0f) ? 0.0f : t);
+		t = t * t * (3.f - 2.f*t); //극솟값이 0, 극댓값이 1, 변곡점이 0.5. (이 조건을 만족한다면 다른 식을 써도 될 듯)
+		ret.x = v1.x + t * (v2.x - v1.x);
+		ret.y = v1.y + t * (v2.y - v1.y);
+		return ret;
+	}
+
+	//반사벡터 iV:반사시킬 벡터, nV:평면의 노말벡터.
+	Vector2 Vector2::Reflect(const Vector2& iV, const Vector2& nV)
+	{
+		return 2.0f * Dot(iV, nV) * iV - iV;
+	}
+
+	//iV:굴절시킬 벡터, nV:평면의 노말벡터. rI:굴절율
+	Vector2 Vector2::Refract(const Vector2& iV, const Vector2& nV, float rI)
+	{
+		float t = Dot(iV, nV);
+		float r = 1.f - (rI*rI) * (1.f - (t*t));
+
+		Vector2 rv;
+		if (r < 0.f) {
+			rv = { 0.f, 0.f };
+		}
+		else {
+			float s = rI * t + sqrt(r);
+			rv = { rI*iV.x - s * nV.x, rI*iV.y - s * nV.y };
+		}
+	}
+
+	const Vector2 Vector2::Zero = { 0.f, 0.f };
+	const Vector2 Vector2::UnitX = { 1.f, 0.f };
+	const Vector2 Vector2::UnitY = { 0.f, 1.f };
 
 	float3::float3()
 	{
@@ -23,6 +248,111 @@ namespace Lypi
 		x = _x; y = _y; z = _z;
 	}
 
+	Vector3::Vector3()
+		: float3() {}
+
+	Vector3::Vector3(float x)
+		: float3(x, x, x) {}
+
+	Vector3::Vector3(float x, float y, float z)
+		: float3(x, y, z) {}
+
+	Vector3::Vector3(const float3& F)
+	{ 
+		x = F.x; 
+		y = F.y; 
+		z = F.z; 
+	}
+
+	Vector3::Vector3(const Vector3& V)
+	{ 
+		x = V.x; 
+		y = V.y; 
+		z = V.z; 
+	}
+
+	bool Vector3::operator== (const Vector3& V) const 
+	{ 
+		if (x == V.x && y == V.y && z == V.z) {
+			return true; 
+		} 
+		return false; 
+	}
+
+	bool Vector3::operator!= (const Vector3& V) const 
+	{ 
+		if (x == V.x && y == V.y && z == V.z) { 
+			return false; 
+		} 
+		return true; 
+	}
+
+	Vector3& Vector3::operator+= (const Vector3& V) 
+	{
+		Vector3 rv = { x += V.x, y += V.y, z += V.z };
+		return rv; 
+	}
+
+	Vector3& Vector3::operator-= (const Vector3& V) 
+	{
+		Vector3 rv = { x -= V.x, y -= V.y, z -= V.z };
+		return rv;
+	}
+
+	Vector3& Vector3::operator*= (float S)
+	{
+		Vector3 rv = { x *= S, y *= S, z *= S };
+		return rv;
+	}
+
+	Vector3& Vector3::operator/= (float S) 
+	{
+		Vector3 rv = { x /= S, y /= S, z /= S };
+		return rv;
+	}
+
+	Vector3 Vector3::operator+ () const 
+	{
+		return *this; 
+	}
+
+	Vector3 Vector3::operator- () const 
+	{
+		return Vector3(-x, -y, -z); 
+	}
+
+	float Vector3::Length() const 
+	{
+		return sqrtf(x*x + y*y + z*z);
+	}
+
+	float Vector3::LengthSquared() const 
+	{
+		return x*x + y*y + z*z;
+	}
+
+	float Vector3::Dot(const Vector3& V) const 
+	{
+		return x*V.x + y*V.y + z*V.z; 
+	}
+
+	Vector3 Vector3::Normalize() 
+	{
+		Vector3 bf = *this; 
+
+		this->x /= Length(); 
+		this->y /= Length(); 
+		this->z /= Length(); 
+
+		return bf; 
+	}	
+
+	void Vector3::Normalize(Vector3& result) const 
+	{
+		result.x /= Length(); 
+		result.y /= Length();
+		result.z /= Length(); 
+	}
 
 	float4::float4()
 	{
@@ -118,6 +448,62 @@ namespace Lypi
 		}
 
 		return false;
+	}
+
+	stdMatrix stdMatrix::operator+ (const stdMatrix& M)
+	{
+		stdMatrix ret;
+
+		for (int i = 0; i < 4; i++) {
+			for (int k = 0; k < 4; k++) {
+				ret.m[i][k] = m[i][k] + M.m[i][k];
+			}
+		}
+
+		return ret;
+	}
+
+	stdMatrix stdMatrix::operator- (const stdMatrix& M)
+	{
+		stdMatrix ret;
+
+		for (int i = 0; i < 4; i++) {
+			for (int k = 0; k < 4; k++) {
+				ret.m[i][k] = m[i][k] - M.m[i][k];
+			}
+		}
+
+		return ret;
+	}
+
+	stdMatrix stdMatrix::operator* (const stdMatrix& M)
+	{
+		stdMatrix ret;
+
+		for (int i = 0; i < 4; i++) {
+			for (int k = 0; k < 4; k++) {
+				ret.m[i][k] =
+					m[i][0] * M.m[0][k] +
+					m[i][1] * M.m[1][k] +
+					m[i][2] * M.m[2][k] +
+					m[i][3] * M.m[3][k];
+			}
+		}
+
+		return ret;
+	}
+
+	stdMatrix stdMatrix::operator* (const float S)
+	{
+		stdMatrix ret;
+
+		for (int i = 0; i < 4; i++) {
+			for (int k = 0; k < 4; k++) {
+				ret.m[i][k] = S * M.m[i][k];
+			}
+		}
+
+		return ret;
 	}
 
 	stdMatrix stdMatrix::operator+= (const stdMatrix& M)
@@ -276,75 +662,6 @@ namespace Lypi
 		result = 1 / det * subMat;
 	}
 
-	//전역 함수
-	stdMatrix stdMatrix::operator+ (const stdMatrix& M)
-	{
-		stdMatrix ret;
 
-		for (int i = 0; i < 4; i++) {
-			for (int k = 0; k < 4; k++) {
-				ret.m[i][k] = m[i][k] + M.m[i][k];
-			}
-		}
-
-		return ret;
-	}
-
-	stdMatrix stdMatrix::operator- (const stdMatrix& M)
-	{
-		stdMatrix ret;
-
-		for (int i = 0; i < 4; i++) {
-			for (int k = 0; k < 4; k++) {
-				ret.m[i][k] = m[i][k] - M.m[i][k];
-			}
-		}
-
-		return ret;
-	}
-
-	stdMatrix stdMatrix::operator* (const stdMatrix& M)
-	{
-		stdMatrix ret;
-
-		for (int i = 0; i < 4; i++) {
-			for (int k = 0; k < 4; k++) {
-				ret.m[i][k] = 
-					m[i][0] * M.m[0][k] +
-					m[i][1] * M.m[1][k] +
-					m[i][2] * M.m[2][k] +
-					m[i][3] * M.m[3][k];
-			}
-		}
-
-		return ret;
-		
-	}
-
-	stdMatrix operator* (float S, const stdMatrix& M)
-	{
-		stdMatrix ret;
-
-		for (int i = 0; i < 4; i++) {
-			for (int k = 0; k < 4; k++) {
-				ret.m[i][k] = S * M.m[i][k];
-			}
-		}
-
-		return ret;
-	}
-
-	stdMatrix operator* (const stdMatrix& M, float S)
-	{
-		stdMatrix ret;
-
-		for (int i = 0; i < 4; i++) {
-			for (int k = 0; k < 4; k++) {
-				ret.m[i][k] = S * M.m[i][k];
-			}
-		}
-
-		return ret;
-	}
 
 }
--- TBasisSample/Library/mathHeader_L.h
+++ TBasisSample/Library/mathHeader_L.h
@@ -12,17 +12,18 @@ using namespace DirectX;
 
 //전방 선언
 struct float2;
+struct Vector2;
+
 struct float3;
+struct Vector3;
+
 struct float4;
+struct Vector4;
 
 struct float2x2;
 struct float3x3;
 struct float4x4;
 struct stdMatrix;
-
-struct Vector2;
-struct Vector3;
-struct Vector4;
 
 struct Quaternion;
 struct Plane;
@@ -48,6 +49,72 @@ namespace Lypi
 		float2(float _x, float _y);
 	};
 
+	struct Vector2 : float2
+	{
+		static const Vector2 Zero;
+		static const Vector2 UnitX;
+		static const Vector2 UnitY;
+
+		//생성자
+		Vector2();
+		explicit Vector2(float x);
+		Vector2(float x, float y);
+		Vector2(const float2& V);
+		Vector2(const Vector2& F);
+
+		//복사 생성자
+		Vector2(const Vector2&) = default;
+		Vector2(Vector2&&) = default;
+
+		//연산자재정의
+		Vector2& operator=(const Vector2&) = default;
+		Vector2& operator=(Vector2&&) = default;
+
+		bool operator== (const Vector2& V) const;
+		bool operator!= (const Vector2& V) const;
+
+		Vector2& operator+= (const Vector2& V);
+		Vector2& operator-= (const Vector2& V);
+		Vector2& operator*= (float S);
+		Vector2& operator/= (float S);
+
+		Vector2 operator+ () const;
+		Vector2 operator- () const;
+
+		float Length() const;
+		float LengthSquared() const;
+
+		float Dot(const Vector2& V) const;
+
+		Vector2 Normalize();
+		void Normalize(Vector2& result) const;
+
+		Vector2 Clamp(const Vector2& min, const Vector2& max);
+		void Clamp(const Vector2& min, const Vector2& max, Vector2& ret);
+
+		//연산자 재정의
+		static friend Vector2 operator+ (const Vector2& V1, const Vector2& V2);
+		static friend Vector2 operator- (const Vector2& V1, const Vector2& V2);
+		static friend Vector2 operator* (float S, const Vector2& V);
+		static friend Vector2 operator* (const Vector2& V, float S);
+		static friend Vector2 operator/ (float S, const Vector2& V);
+		static friend Vector2 operator/ (const Vector2& V, float S);
+
+		//static function
+		static float Dot(const Vector2& v1, const Vector2& v2);
+		static float Distance(const Vector2& v1, const Vector2& v2);
+		static float DistanceSquared(const Vector2& v1, const Vector2& v2);
+
+		static Vector2 Min(const Vector2& v1, const Vector2& v2);
+		static Vector2 Max(const Vector2& v1, const Vector2& v2);
+
+		static Vector2 Lerp(const Vector2& v1, const Vector2& v2, float t); //선형 보간법
+		static Vector2 SmoothStep(const Vector2& v1, const Vector2& v2, float t); //스무스스텝. (필요한가..?)
+
+		static Vector2 Reflect(const Vector2& iV, const Vector2& nV); //iV:반사시킬 벡터, nV:평면의 노말벡터.
+		static Vector2 Refract(const Vector2& iV, const Vector2& nV, float rI); //iV:굴절시킬 벡터, nV:평면의 노말벡터. rI:굴절율
+	};
+
 	struct float3
 	{
 		union {
@@ -59,264 +126,6 @@ namespace Lypi
 		float3(float _x, float _y, float _z);
 	};
 
-	struct float4
-	{
-		union {
-			struct { float x, y, z, w; };
-			float f[4];
-		};
-
-		float4();
-		float4(float _x, float _y, float _z, float _w);
-	};
-
-	struct float2x2
-	{
-		union {
-			struct {
-				float _11, _12;
-				float _21, _22;
-			};
-			float m[2][2];
-			float2 _1, _2;
-		};
-
-		float2x2();
-		float2x2(float2 __1, float2 __2);
-
-		//복사연산자 기본값으로 정의
-		float2x2(const float2x2&) = default;
-		float2x2(float2x2&&) = default;
-
-		//대입연산자 기본값으로 정의
-		float2x2& operator= (const float2x2&) = default;
-		float2x2& operator= (float2x2&&) = default;
-	};
-
-	struct float3x3
-	{
-		union {
-			struct {
-				float _11, _12, _13;
-				float _21, _22, _23;
-				float _31, _32, _33;
-			};
-			float m[3][3];
-			float3 _1, _2, _3;
-		};
-
-		float3x3();
-		float3x3(float3 __1, float3 __2, float3 __3);
-
-		//복사연산자 기본값으로 정의
-		float3x3(const float3x3&) = default;
-		float3x3(float3x3&&) = default;
-
-		//대입연산자 기본값으로 정의
-		float3x3& operator= (const float3x3&) = default;
-		float3x3& operator= (float3x3&&) = default;
-	};
-
-
-	struct float4x4
-	{
-		union {
-			struct {
-				float _11, _12, _13, _14;
-				float _21, _22, _23, _24;
-				float _31, _32, _33, _34;
-				float _41, _42, _43, _44;
-			};
-			float m[4][4];
-			float4 _1, _2, _3, _4;
-		};
-
-		float4x4();
-		float4x4(float4 __1, float4 __2, float4 __3, float4 __4);
-
-		//복사연산자 기본값으로 정의
-		float4x4(const float4x4&) = default;
-		float4x4(float4x4&&) = default;
-
-		//대입연산자 기본값으로 정의
-		float4x4& operator= (const float4x4&) = default;
-		float4x4& operator= (float4x4&&) = default;
-	};
-	
-
-	struct stdMatrix : float4x4
-	{
-		static const stdMatrix Identity;
-
-		//생성자 
-		stdMatrix() { *this = Identity; }
-		stdMatrix(float4 __1, float4 __2, float4 __3, float4 __4);
-
-		//method
-		bool operator== (const stdMatrix& M) const;
-		bool operator!= (const stdMatrix& M) const;
-
-		stdMatrix operator+= (const stdMatrix& M);
-		stdMatrix operator-= (const stdMatrix& M);
-
-		stdMatrix operator+ (const stdMatrix& M);
-		stdMatrix operator- (const stdMatrix& M);
-		stdMatrix operator* (const stdMatrix& M);
-
-		Vector3 operator* (const Vector3& M);
-
-		stdMatrix Transpose();
-		void Transpose(stdMatrix& result);
-
-		float deteminant();
-		stdMatrix Inverse();
-		void Inverse(stdMatrix& result);
-
-		//static
-		static friend stdMatrix operator* (float S, const stdMatrix& M);
-		static friend stdMatrix operator* (const stdMatrix& M, float S);	
-
-	};
-
-	struct Vector2 : float2
-	{
-		static const Vector2 Zero;
-		static const Vector2 UnitX;
-		static const Vector2 UnitY;
-
-		//생성자
-		Vector2() : float2() {}
-		explicit Vector2(float x) : float2(x, x) {} //explicit : 자동 형변환 금지.
-		Vector2(float x, float y) : float2(x, y) {}
-		Vector2(float2 V) { x = V.x; y = V.y; }
-		Vector2(const float2& V) { this->x = V.x; this->y = V.y; }
-
-		explicit Vector2(const float2& F) { this->x = F.f[0]; this->y = F.f[1]; } // XMVECTORF32 : 실수 4개를 갖는 벡터. 
-
-		//복사 생성자
-		Vector2(const Vector2&) = default; //디폴트 복사 생성자를 생성할 것.
-		Vector2(Vector2&&) = default;      //&& : 우측값 참조, 우측값을 인자로 받는 디폴트 복사 생성자를 생성할 것.
-
-		//연산자재정의
-		Vector2& operator=(const Vector2&) = default;  //디폴트 대입 연산자를 재정의할 것.
-		Vector2& operator=(Vector2&&) = default;       //우측값을 인자로 받는 대입 연산자를 재정의할 것.
-
-		bool operator== (const Vector2& V) const { if (x == V.x && y == V.y) { return true; } return false; }
-		bool operator!= (const Vector2& V) const { if (x == V.x && y == V.y) { return false; } return true; }
-
-		Vector2& operator+= (const Vector2& V) { this->x += V.x; this->y += V.y; Vector2 rv(this->x, this->y); return rv; }
-		Vector2& operator-= (const Vector2& V) { this->x -= V.x; this->y -= V.y; Vector2 rv(this->x, this->y); return rv; }
-
-		Vector2& operator*= (float S) { this->x *= S; this->y *= S; Vector2 rv(this->x, this->y); return rv; }
-		Vector2& operator/= (float S) { this->x /= S; this->y /= S; Vector2 rv(this->x, this->y); return rv; }
-
-		Vector2 operator+ () const { return *this; }
-		Vector2 operator- () const { return Vector2(-x, -y); }
-
-		float Length() const { return sqrtf(x*x + y * y); }
-		float LengthSquared() const { return x * x + y * y; }
-
-		float Dot(const Vector2& V) const { return this->x * V.x + this->y * V.y; }
-
-		Vector2 Normalize() { Vector2 bf = *this; this->x /= Length(); this->y /= Length(); return bf; }    //자기자신을 바꾸고 이전값을 리턴.
-		void Normalize(Vector2& result) const { result.x /= Length(); result.y /= Length(); }              //바꾼 값을 인자에 넣고 자기자신은 보존.
-
-		Vector2 Clamp(const Vector2& min, const Vector2& max) //자기자신을 바꾸고 이전값을 리턴.
-		{
-			Vector2 bf = *this;
-			(x <= min.x) ? (x = min.x) : ((x > max.x) ? (x = max.x) : (x));
-			(y <= min.y) ? (y = min.y) : ((y > max.y) ? (y = max.y) : (y));
-			return bf;
-		}
-		void Clamp(const Vector2& min, const Vector2& max, Vector2& ret)  //바꾼 값을 인자에 넣고 자기자신은 보존.
-		{
-			(ret.x <= min.x) ? (ret.x = min.x) : ((ret.x > max.x) ? (ret.x = max.x) : (ret.x));
-			(ret.y <= min.y) ? (ret.y = min.y) : ((ret.y > max.y) ? (ret.y = max.y) : (ret.y));
-		}
-
-		//Vector2 Scale(const stdMatrix& m) // 자기자신을 바꾸고 이전값을 리턴
-		//{
-		//	Vector2 bf = *this;
-
-		//	return bf;
-		//	
-		//}
-
-		//연산자 재정의
-		static friend Vector2 operator+ (const Vector2& V1, const Vector2& V2) { Vector2 rv; rv.x = V1.x + V2.x; rv.y = V1.y + V2.y; return rv; }
-		static friend Vector2 operator- (const Vector2& V1, const Vector2& V2) { Vector2 rv; rv.x = V1.x - V2.x; rv.y = V1.y - V2.y; return rv; }
-		static friend Vector2 operator* (float S, const Vector2& V) { Vector2 rv; rv.x *= S; rv.y *= S; return rv; }
-		static friend Vector2 operator* (const Vector2& V, float S) { Vector2 rv; rv.x *= S; rv.y *= S; return rv; }
-		static friend Vector2 operator/ (float S, const Vector2& V) { Vector2 rv; rv.x /= S; rv.y /= S; return rv; }
-		static friend Vector2 operator/ (const Vector2& V, float S) { Vector2 rv; rv.x /= S; rv.y /= S; return rv; }
-
-		//static function
-		static float Dot(const Vector2& v1, const Vector2& v2) { return v1.x * v2.x + v1.y * v2.y; }
-
-		static float Distance(const Vector2& v1, const Vector2& v2) { return (v2 - v1).Length(); }
-		static float DistanceSquared(const Vector2& v1, const Vector2& v2) { return (v2 - v1).LengthSquared(); }
-
-		static Vector2 Min(const Vector2& v1, const Vector2& v2)
-		{
-			Vector2 ret;
-			(v1.x <= v2.x) ? (ret.x = v1.x) : (ret.x = v2.x);
-			(v1.y <= v2.y) ? (ret.y = v1.y) : (ret.y = v2.y);
-			return ret;
-		}
-
-		static Vector2 Max(const Vector2& v1, const Vector2& v2)
-		{
-			Vector2 ret;
-			(v1.x >= v2.x) ? (ret.x = v1.x) : (ret.x = v2.x);
-			(v1.y >= v2.y) ? (ret.y = v1.y) : (ret.y = v2.y);
-			return ret;
-		}
-
-		//선형 보간법
-		static Vector2 Lerp(const Vector2& v1, const Vector2& v2, float t)
-		{
-			Vector2 ret;
-			t = (t > 1.0f) ? 1.0f : ((t < 0.0f) ? 0.0f : t);
-			ret.x = v1.x + t * (v2.x - v1.x);
-			ret.y = v1.y + t * (v2.y - v1.y);
-			return ret;
-		}
-
-		//스무스스텝. (필요한가..?)
-		static Vector2 SmoothStep(const Vector2& v1, const Vector2& v2, float t)
-		{
-			Vector2 ret;
-			t = (t > 1.0f) ? 1.0f : ((t < 0.0f) ? 0.0f : t);
-			t = t * t * (3.f - 2.f*t); //극솟값이 0, 극댓값이 1, 변곡점이 0.5. (이 조건을 만족한다면 다른 식을 써도 될 듯)
-			ret.x = v1.x + t * (v2.x - v1.x);
-			ret.y = v1.y + t * (v2.y - v1.y);
-			return ret;
-		}
-
-		//반사벡터 iV:반사시킬 벡터, nV:평면의 노말벡터.
-		static Vector2 Reflect(const Vector2& iV, const Vector2& nV)
-		{ return 2.0f * Dot(iV, nV) * iV - iV; }
-
-		static Vector2 Refract(const Vector2& iV, const Vector2& nV, float rI)
-		{
-			float t = Dot(iV, nV);
-			float r = 1.f - (rI*rI) * (1.f - (t*t));
-
-			Vector2 rv;
-			if (r < 0.f) {
-				rv = { 0.f, 0.f }; 
-			}
-			else { 
-				float s = rI * t + sqrt(r);
-				rv = { rI*iV.x - s*nV.x, rI*iV.y - s*nV.y };
-			}
-		}
-	};
-
-	const Vector2 Vector2::Zero = { 0.f, 0.f };
-	const Vector2 Vector2::UnitX = { 1.f, 0.f };
-	const Vector2 Vector2::UnitY = { 0.f, 1.f };
-
 	struct Vector3 : float3
 	{
 		static const Vector3 Zero;
@@ -325,41 +134,39 @@ namespace Lypi
 		static const Vector3 UnitZ;
 
 		//생성자
-		Vector3() : float3() {}
-		explicit Vector3(float x) : float3(x, x, x) {} //explicit : 자동 형변환 금지.
-		Vector3(float x, float y, float z) : float3(x, y, z) {}
-		Vector3(float3 V) { x = V.x; y = V.y; z = V.z; }
-		Vector3(const float3& V) { this->x = V.x; this->y = V.y; this->z = V.z; }
-
-		explicit Vector3(const float3& F) { this->x = F.f[0]; this->y = F.f[1]; this->z = F.f[2]; } 
+		Vector3();
+		explicit Vector3(float x);
+		Vector3(float x, float y, float z);
+		Vector3(const float3& F);
+		Vector3(const Vector3& V);
 
 		//복사 생성자
-		Vector3(const Vector3&) = default; //디폴트 복사 생성자를 생성할 것.
-		Vector3(Vector3&&) = default;      //&& : 우측값 참조, 우측값을 인자로 받는 디폴트 복사 생성자를 생성할 것.
+		Vector3(const Vector3&) = default;
+		Vector3(Vector3&&) = default;     
 
 		//연산자재정의
-		Vector3& operator=(const Vector3&) = default;  //디폴트 대입 연산자를 재정의할 것.
-		Vector3& operator=(Vector3&&) = default;       //우측값을 인자로 받는 대입 연산자를 재정의할 것.
+		Vector3& operator=(const Vector3&) = default;
+		Vector3& operator=(Vector3&&) = default;
 
-		bool operator== (const Vector3& V) const { if (x == V.x && y == V.y && z == V.z) { return true; } return false; }
-		bool operator!= (const Vector3& V) const { if (x == V.x && y == V.y && z == V.z) { return false; } return true; }
+		bool operator== (const Vector3& V) const;
+		bool operator!= (const Vector3& V) const;
 
-		Vector3& operator+= (const Vector3& V) { this->x += V.x; this->y += V.y; this->z += V.z; Vector3 rv(this->x, this->y, this->z); return rv; }
-		Vector3& operator-= (const Vector3& V) { this->x -= V.x; this->y -= V.y; this->z -= V.z; Vector3 rv(this->x, this->y, this->z); return rv; }
+		Vector3& operator+= (const Vector3& V);
+		Vector3& operator-= (const Vector3& V);
 
-		Vector3& operator*= (float S) { this->x *= S; this->y *= S; this->z *= S; Vector3 rv(this->x, this->y, this->z); return rv; }
-		Vector3& operator/= (float S) { this->x /= S; this->y /= S; this->z /= S; Vector3 rv(this->x, this->y, this->z); return rv; }
+		Vector3& operator*= (float S);
+		Vector3& operator/= (float S);
 
-		Vector3 operator+ () const { return *this; }
-		Vector3 operator- () const { return Vector3(-x, -y, -z); }
+		Vector3 operator+ () const;
+		Vector3 operator- () const;
 
-		float Length() const { return sqrtf(x*x + y*y + z*z); }
-		float LengthSquared() const { return x*x + y*y + z*z; }
+		float Length() const;
+		float LengthSquared() const;
 
-		float Dot(const Vector3& V) const { return this->x*V.x + this->y*V.y + this->z*V.z; }
+		float Dot(const Vector3& V) const;
 
-		Vector3 Normalize() { Vector3 bf = *this; this->x /= Length(); this->y /= Length(); this->z /= Length(); return bf; }    //자기자신을 바꾸고 이전값을 리턴.
-		void Normalize(Vector3& result) const { result.x /= Length(); result.y /= Length(); result.z /= Length(); }              //바꾼 값을 인자에 넣고 자기자신은 보존.
+		Vector3 Normalize();
+		void Normalize(Vector3& result) const;
 
 		Vector3 Clamp(const Vector3& min, const Vector3& max) //자기자신을 바꾸고 이전값을 리턴.
 		{
@@ -456,7 +263,7 @@ namespace Lypi
 			}
 			else {
 				float s = rI * t + sqrt(r);
-				rv = { rI*iV.x - s*nV.x, rI*iV.y - s*nV.y, rI*iV.z - s*nV.z };
+				rv = { rI*iV.x - s * nV.x, rI*iV.y - s * nV.y, rI*iV.z - s * nV.z };
 			}
 		}
 
@@ -466,9 +273,127 @@ namespace Lypi
 		}
 	};
 
-}
+	struct float4
+	{
+		union {
+			struct { float x, y, z, w; };
+			float f[4];
+		};
 
-const Vector3 Vector3::Zero = { 0.f, 0.f, 0.f };
-const Vector3 Vector3::UnitX = { 1.f, 0.f, 0.f };
-const Vector3 Vector3::UnitY = { 0.f, 1.f, 0.f };
-const Vector3 Vector3::UnitZ = { 0.f, 0.f, 1.f };
+		float4();
+		float4(float _x, float _y, float _z, float _w);
+	};
+
+	const Vector3 Vector3::Zero = { 0.f, 0.f, 0.f };
+	const Vector3 Vector3::UnitX = { 1.f, 0.f, 0.f };
+	const Vector3 Vector3::UnitY = { 0.f, 1.f, 0.f };
+	const Vector3 Vector3::UnitZ = { 0.f, 0.f, 1.f };
+
+	struct float2x2
+	{
+		union {
+			struct {
+				float _11, _12;
+				float _21, _22;
+			};
+			float m[2][2];
+			float2 _1, _2;
+		};
+
+		float2x2();
+		float2x2(float2 __1, float2 __2);
+
+		//복사연산자 기본값으로 정의
+		float2x2(const float2x2&) = default;
+		float2x2(float2x2&&) = default;
+
+		//대입연산자 기본값으로 정의
+		float2x2& operator= (const float2x2&) = default;
+		float2x2& operator= (float2x2&&) = default;
+	};
+
+	struct float3x3
+	{
+		union {
+			struct {
+				float _11, _12, _13;
+				float _21, _22, _23;
+				float _31, _32, _33;
+			};
+			float m[3][3];
+			float3 _1, _2, _3;
+		};
+
+		float3x3();
+		float3x3(float3 __1, float3 __2, float3 __3);
+
+		//복사연산자 기본값으로 정의
+		float3x3(const float3x3&) = default;
+		float3x3(float3x3&&) = default;
+
+		//대입연산자 기본값으로 정의
+		float3x3& operator= (const float3x3&) = default;
+		float3x3& operator= (float3x3&&) = default;
+	};
+
+
+	struct float4x4
+	{
+		union {
+			struct {
+				float _11, _12, _13, _14;
+				float _21, _22, _23, _24;
+				float _31, _32, _33, _34;
+				float _41, _42, _43, _44;
+			};
+			float m[4][4];
+			float4 _1, _2, _3, _4;
+		};
+
+		float4x4();
+		float4x4(float4 __1, float4 __2, float4 __3, float4 __4);
+
+		//복사연산자 기본값으로 정의
+		float4x4(const float4x4&) = default;
+		float4x4(float4x4&&) = default;
+
+		//대입연산자 기본값으로 정의
+		float4x4& operator= (const float4x4&) = default;
+		float4x4& operator= (float4x4&&) = default;
+	};
+
+
+	struct stdMatrix : float4x4
+	{
+		static const stdMatrix Identity;
+
+		//생성자 
+		stdMatrix() { *this = Identity; }
+		stdMatrix(float4 __1, float4 __2, float4 __3, float4 __4);
+
+		//method
+		bool operator== (const stdMatrix& M) const;
+		bool operator!= (const stdMatrix& M) const;
+
+		stdMatrix operator+= (const stdMatrix& M);
+		stdMatrix operator-= (const stdMatrix& M);
+
+		stdMatrix operator+ (const stdMatrix& M);
+		stdMatrix operator- (const stdMatrix& M);
+		stdMatrix operator* (const stdMatrix& M);
+		stdMatrix operator* (const float S);
+
+		stdMatrix Transpose();
+		void Transpose(stdMatrix& result);
+
+		float deteminant();
+		stdMatrix Inverse();
+		void Inverse(stdMatrix& result);
+
+		stdMatrix Translation(const Vector3& V);
+		void Translation(const Vector3& V, stdMatrix& result);
+
+
+	};
+
+}
